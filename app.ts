0b851fcb9c963295b0044ce50aae18606742dfe5
Pascal Garber <pascal@jumplink.eu>
2016-01-31 17:38:24 +0100

    [Theme] Outsourced routes to express-theme

--- app.ts
+++ app.ts
@@ -1,38 +1,31 @@
+
 import * as express from "express";
 import * as path from "path";
 import * as favicon from "serve-favicon";
 import * as logger from "morgan";
+
+import * as ExpressTheme from './node_modules/express-theme/express-theme.ts';
+
 import cookieParser = require('cookie-parser');
 import bodyParser = require('body-parser');
 
-import * as sassMiddleware from "node-sass-middleware";
-
-import routes = require('./routes/index');
-import users = require('./routes/users');
 
 var app = express();
+
+var theme = new ExpressTheme.Theme("test");
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  //debug: true,
-  indentedSyntax: true,
-  sourceMap: true
-}));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use(theme.router);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
